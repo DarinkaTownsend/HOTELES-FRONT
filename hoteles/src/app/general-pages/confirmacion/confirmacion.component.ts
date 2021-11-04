277bb2270a6b682dbb411d1ca3e8be1e5ad3f433
DarinkaTownsend <darinka.townsend@hotmail.com>
2021-11-03 22:37:24 -0500

    Confirmacion

--- hoteles/src/app/general-pages/confirmacion/confirmacion.component.ts
+++ hoteles/src/app/general-pages/confirmacion/confirmacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-confirmacion',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirmacion.component.css']
 })
 export class ConfirmacionComponent implements OnInit {
+  miFormulario3: FormGroup;
+  constructor() {
+    this.miFormulario3 = new FormGroup({
+      'usuario': new FormControl(),
+      'name': new FormControl(),
+      'apellido': new FormControl(),
+      'contrasena': new FormControl(),
+      'contrasena2': new FormControl(),
+      'correo': new FormControl()
+    });
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
 
+  Reservar(){}
 }
