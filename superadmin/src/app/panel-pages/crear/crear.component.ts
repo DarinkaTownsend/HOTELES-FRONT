27f729f087d9bb09cc2f35ecbc27e5bc0901d1bc
DarinkaTownsend <darinka.townsend@hotmail.com>
2021-11-04 16:35:46 -0500

    cambios en agregar admin

--- superadmin/src/app/panel-pages/crear/crear.component.ts
+++ superadmin/src/app/panel-pages/crear/crear.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsuariosService } from '../../service/usuarios.service'
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+
+
 
 @Component({
   selector: 'app-crear',
@@ -6,10 +13,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear.component.css']
 })
 export class CrearComponent implements OnInit {
+  miFormulario6: FormGroup;
 
-  constructor() { }
+  constructor(private usuariosService : UsuariosService,private router: Router) {
+    this.miFormulario6 = new FormGroup({
+      'usuario': new FormControl(),
+      'name': new FormControl(),
+      'apellido': new FormControl(),
+      'contrasena': new FormControl(),
+      'contrasena2': new FormControl(),
+      'correo': new FormControl()
+    });
+
+
+  }
 
   ngOnInit(): void {
+  }
+
+  miSubmit() {
+    console.log(this.miFormulario6.value);
+
+    const elUser = {"username": this.miFormulario6.value.usuario,
+                    "email" : this.miFormulario6.value.correo,
+                    "password1": this.miFormulario6.value.contrasena,
+                    "password2": this.miFormulario6.value.contrasena2,
+    }
+    this.usuariosService.addUsuarios(elUser).subscribe(
+      res  => {
+        var x= JSON.stringify(res)
+        var y = JSON.parse(x)
+
+        //console.log(y.message);
+        Swal.fire({
+          title: y.message,
+          //text: y.message,
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        })
+
+
+        //this.router.navigate(['/admin']);
+      },
+      err  => {
+        console.log(err)
+        Swal.fire({
+          title: 'Error!',
+          text: err.error.message,
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+      }
+
+
+    )
+
   }
 
 }
