92674f8868948fe3e557e1040a71f3ad4929279d
DarinkaTownsend <darinka.townsend@hotmail.com>
2021-11-04 14:53:14 -0500

    cambios super admin

--- superadmin/src/app/panel-pages/mis-hoteles/mis-hoteles.component.ts
+++ superadmin/src/app/panel-pages/mis-hoteles/mis-hoteles.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-hoteles.component.css']
 })
 export class MisHotelesComponent implements OnInit {
-
+  hoteles:any =[];
   constructor() { }
 
   ngOnInit(): void {
-  }
+
+            fetch("http://127.0.0.1:8000/api/hotels/")
+            .then(data => data.json())
+            .then(data => {
+              console.log(this.hoteles)
+              this.hoteles=data
+              /*
+              let tbody = document.getElementById("ordenes_tablas")
+
+              if (tbody) {
+                tbody.innerHTML = "";
+
+                for(let hotel of this.hoteles){
+                  let row =
+                    `<tr>
+                      <td>${hotel["id_hotel"]}</td>
+                      <td>${hotel["name"]}</td>
+                      <td>${hotel["address"]}</td>
+                      <td>${hotel["create_at"]}</td>
+                      <td>${hotel["update_at"]}</td>
+                    </tr>`
+                  tbody.innerHTML += row;
+                }
+
+              }
+               */
+            });
+
+
+
+      }
 
 }
