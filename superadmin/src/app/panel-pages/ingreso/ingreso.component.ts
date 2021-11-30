7d6562d3f5948479a660b01abaf83a36042bb3c2
DarinkaTownsend <darinka.townsend@hotmail.com>
2021-11-29 22:06:42 -0500

    cambios

--- superadmin/src/app/panel-pages/ingreso/ingreso.component.ts
+++ superadmin/src/app/panel-pages/ingreso/ingreso.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators,FormBuilder } from '@angular/forms';
 import { UsuariosService } from '../../service/usuarios.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+
 
 
 
@@ -13,42 +17,108 @@ import Swal from 'sweetalert2';
   styleUrls: ['./ingreso.component.css']
 })
 export class IngresoComponent implements OnInit {
-  usuario_viejos:any =[];
-  miFormulario5: FormGroup;
+  signupForm: FormGroup;
+  data:any=[];
+  accesoU={
+    access_token: "",
+    refresh_token: "",
+    user: {
+        pk: 0,
+        username: "",
+        email: "",
+        first_name: "",
+        last_name: ""
+    }
+}
 
-  constructor(private usuariosService : UsuariosService,private router: Router) {
-      //se obtienen los datos del sign in
-      this.miFormulario5 = new FormGroup({
-        'usuario': new FormControl(),
-        'contrasena': new FormControl()
-      });
-   }
+
+
+  constructor(private _builer: FormBuilder,private router: Router,private usuariosService : UsuariosService){
+      this.signupForm=this._builer.group({
+        usuario: ['',Validators.required] ,
+        contrasena:['',Validators.required] ,
+      })
+    }
+
+
 
   ngOnInit(): void {
   }
 
 
 
-  miSubmit2() {
-    console.log(this.miFormulario5.value);
-    console.log(this.miFormulario5 );
-    const elUser = {"usuario": this.miFormulario5.value.usuario,
-                    "email": "",
-                    "password": this.miFormulario5.value.contrasena,
-                    "is_client": false,
+  enviar(values: any) {
+    const elUser = {"username":this.signupForm.value.usuario ,
+                    "email":"" ,
+                    "password": this.signupForm.value.contrasena
     }
 
-    if(elUser.usuario!="darinka2000"){
+    const elUser2 = {"username":this.signupForm.value.usuario ,
+    "email":"" ,
+    "password": this.signupForm.value.contrasena,
+    "nombre":"",
+    "apellido":"",
+    "pk":0
+    }
+
+    if(this.signupForm.value.usuario==""||this.signupForm.value.contrasena==""){
       Swal.fire({
-        title: 'Error!',
+        title: "Contraseña o Usuario incorrecto",
         icon: 'error',
-        confirmButtonText: 'Aceptar'
+        confirmButtonText: 'Volver a intentar'
       })
     }else{
 
+      console.log(this.data)
+      this.usuariosService.login(elUser).subscribe(
+        res  => {
+
+          localStorage.setItem('datosF2',JSON.stringify(res));
+          this.router.navigate(['/inicio']);
+        },
+        err  => {
+
+          Swal.fire({
+            title: 'Error!',
+            text: err.error.message,
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+          })
+        }
+
+      )
+
+    }
+/*
+      this.router.navigate(['/inicio']);
+      fetch("https://sadminhoteles.pythonanywhere.com/api/authentication/login/"+elUser)
+      .then(data=>data.json())
+      .then(data=>{
+
+        this.accesoU.access_token=data.access_token;
+        this.accesoU.refresh_token=data.refresh_token;
+        this.accesoU.user.username=data.user.username;
+        this.accesoU.user.pk=data.user.pk;
+        this.accesoU.user.email=data.user.email;
+        this.accesoU.user.first_name=data.user.first_name;
+        this.accesoU.user.last_name=data.user.last_name;
+
+      })
+
+      console.log(this.accesoU);
 
 
-    fetch("http://127.0.0.1:8000/api/user_type/"+elUser.usuario)
+
+
+
+
+
+
+
+
+
+
+    fetch("http://sadminhoteles.pythonanywhere.com/api/user_type/"+this.accesoU.user.pk)
     .then(data=>data.json())
       .then(data=>{
         this.usuario_viejos=data;
@@ -75,7 +145,8 @@ export class IngresoComponent implements OnInit {
       }
 
     )
-  }
-  }
 
+
+*/
+}
 }
