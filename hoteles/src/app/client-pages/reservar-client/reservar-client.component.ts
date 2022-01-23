13ad0a57ff0c0a71d2ffeb17cac51faf1700128d
DarinkaTownsend <darinka.townsend@hotmail.com>
2022-01-23 16:03:08 -0500

    muchos cambios

--- hoteles/src/app/client-pages/reservar-client/reservar-client.component.ts
+++ hoteles/src/app/client-pages/reservar-client/reservar-client.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservar-client.component.css']
 })
 export class ReservarClientComponent implements OnInit {
-
+  rooms: any = [];
+  personask: any = "";
   constructor() { }
 
+  nombreHS:any=""
+  numeroPerS:any=""
+  numeroCS:any=""
+  desayunoS:any=""
+  cocinaS:any=""
+  wifiS:any=""
+  lavadoraS:any=""
+  precioS:any=""
+
+
   ngOnInit(): void {
+    this.personask = localStorage.getItem('personask');
+
+    this.nombreHS=localStorage.getItem("nombreHS")
+    this.numeroPerS=localStorage.getItem("numeroPerS")
+    this.numeroCS=localStorage.getItem("numeroCS")
+    this.desayunoS=localStorage.getItem("desayunoS")
+    this.cocinaS=localStorage.getItem("cocinaS")
+    this.wifiS=localStorage.getItem("wifiS")
+    this.lavadoraS=localStorage.getItem("lavadoraS")
+    this.precioS=localStorage.getItem("precioS")
+
   }
 
 }
